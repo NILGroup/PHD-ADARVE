3edbff6649450a5842c9c8d4ccdaac002c8187f4
Alejandro Villar Rubio <alvill04@ucm.es>
2025-09-29 14:32:15 +0200

    Arreglada las Quests del primer tutorial

---
+++ RadiationRoom/Unreal/Source/RadiationRoom/Public/WorldObject.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "UObject/NoExportTypes.h"
+#include "WorldObject.generated.h"
+
+/**
+ * 
+ */
+UCLASS(Blueprintable, Meta=(ShowWorldContextPin))
+class RADIATIONROOM_API UWorldObject : public UObject
+{
+	GENERATED_BODY()
+	
+};
